89a64b3f99248c125ee0d65ec9593329f856205f
Andres Cabrera <andrescabsi@gmail.com>
2014-12-16 20:34:06 -0500

    fix

--- js/index.js
+++ js/index.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
 
+
+$('#carousel-example-generic').carousel()
+
+	owl_ppal();
+	owl_testimonial();
+	fixes_slider_ppal();
+	console.log("Hola");
+});//End doc ready
+
+function owl_ppal(){
 	var owl = $("#main_courses_slider"); // save reference to variable
   	owl.owlCarousel({
         items:1,
         loop: true,
         center: true,
-        URLhashListener:true,
+        URLhashListener: true,
         startPosition: 'URLHash',
         autoplay: true,
 	    nav: true,
@@ -16,7 +26,23 @@ $(document).ready(function(){
     });
 
 
-    // Navigation Events
+
+
+	var hash = window.location.hash.substr(1);
+	console.log(hash); // alerts the hash tag at the end of the URL
+	console.log(hash.length);
+	if (hash.length >= 0) {
+		console.log("Verdaderooo");
+		owl.trigger('owl.jumpTo', hash)
+	};
+
+
+
+
+
+}
+// Navigation Events
+function owl_testimonial(){
     $('#testimonials_slider').owlCarousel({
     	items:1,
 	    loop:true,
@@ -26,20 +52,21 @@ $(document).ready(function(){
 	    dots: true,
 	    autoplayTimeout: 10000,
 	});
+}
 
+function fixes_slider_ppal(){
 	//Add classes each dot
 	$("#main_courses_slider .owl-dot").attr('id', function(i) {
 			return 'slide'+(i+1);
 	});
-
 	//Print slide name
 	$("#main_courses_slider #slide1").prepend('<p>Cursos</p>');
 	$("#main_courses_slider #slide2").prepend('<p>Básico</p>');
 	$("#main_courses_slider #slide3").prepend('<p>Gráficas</p>');
 	$("#main_courses_slider #slide4").prepend('<p>Tablas Dinámicas</p>');
 	$("#main_courses_slider #slide5").prepend('<p>Macros</p>');
+}
+	
 
 
 
-
-});//End doc ready
